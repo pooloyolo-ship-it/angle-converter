afd449808486ffc8dac1915322239e55dd1b8d3a
pooloyolo-ship-it <pooloyolo@gmail.com>
2025-09-03 20:23:36 +0900

    Stage All

--- resection/service-worker.js
+++ resection/service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',(e)=>{e.waitUntil(caches.open('resection-white-full-v1').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+self.addEventListener('install',e=>{e.waitUntil(caches.open('resection-white-v164-final2').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
